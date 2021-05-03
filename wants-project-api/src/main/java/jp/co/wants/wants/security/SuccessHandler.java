f09b5a71a6e2852e4f379c910bc42d1810830b72
su-san1129 <deviationvalue1023@outlook.jp>
2021-05-03 18:23:58 +0900

    ユーザー情報をレスポンスに含めるように修正

--- wants-project-api/src/main/java/jp/co/wants/wants/security/SuccessHandler.java
+++ wants-project-api/src/main/java/jp/co/wants/wants/security/SuccessHandler.java
@@ -1,5 +1,6 @@
 package jp.co.wants.wants.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jp.co.wants.wants.IdGenerator;
 import jp.co.wants.wants.domain.LoginUser;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,23 +15,30 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @Component
 public class SuccessHandler implements AuthenticationSuccessHandler {
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         final var principal = (LoginUser) authentication.getPrincipal();
         // TODO: tokenのセットを行う
         String token = JwtTokenGenerator.generate(principal);
         response.setHeader("Authorization", String.format("Bearer %s", token));
-        response.setHeader("user", principal.getUsername());
+
+        // ユーザー情報をレスポンスに含める
+        PrintWriter writer = response.getWriter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        writer.println(objectMapper.writeValueAsString(principal.getUser()));
+        writer.flush();
+        writer.close();
         clearAuthentication(request);
     }
 
-    private void clearAuthentication(HttpServletRequest request){
+    private void clearAuthentication(HttpServletRequest request) {
         HttpSession session = request.getSession(false);
-        if(session == null){
+        if (session == null) {
             return;
         }
         session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
